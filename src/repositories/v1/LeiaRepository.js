a1f4216ef681c21be153b0e1d59f034eeff31ed9
Mohamed Ahmed <mohamedtaha2005@gmail.com>
2026-05-01 23:43:29 +0200

    feat: implement regex search functionality in LeiaRepository

--- src/repositories/v1/LeiaRepository.js
+++ src/repositories/v1/LeiaRepository.js
@@ -1,6 +1,7 @@
 import Leia from '../../models/Leia.js';
 import { aggregateFindLatestVersions } from '../../utils/aggregates.js';
 import { applyVisibilityFilters } from '../../utils/entity.js';
+import { regexQuery } from '../../utils/search.js';
 
 class LeiaRepository {
   // READ METHODS
@@ -86,7 +87,7 @@ class LeiaRepository {
 
     // Add text and apiVersion filters to query
     if (text) {
-      query['$text'] = { $search: text };
+      Object.assign(query, regexQuery(text, 'metadata.name'));
     }
     if (apiVersion) {
       query['apiVersion'] = apiVersion;
