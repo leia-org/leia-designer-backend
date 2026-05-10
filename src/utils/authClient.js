f13c6b8236fcaa1e43e03b9931127d39c8515016
Miguel Regidor García <mregidorgarcia@gmail.com>
2026-05-11 00:53:36 +0200

    fix: saving designer models bug with users

--- src/utils/authClient.js
+++ src/utils/authClient.js
@@ -5,7 +5,7 @@ export const getUserProfileFromAuthService = async (userId) => {
   if (!userId) return null;
 
   try {
-    const response = await axios.get(`${process.env.AUTH_SERVICE_URL}/api/v1/users/${userId}`, {
+    const response = await axios.get(`${process.env.AUTH_SERVICE_URL}/api/v1/users/intern/${userId}`, {
       headers: {
         'x-intern-token': process.env.INTERN_TOKEN
       }
