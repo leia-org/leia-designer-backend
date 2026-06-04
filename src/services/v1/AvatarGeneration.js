30cfb23da9c23fce8ed59c35b2e1b2757286bb7f
Mohamed Ahmed <mohamedtaha2005@gmail.com>
2026-06-04 18:56:54 +0200

    feat(avatar-generation): implement AvatarGeneration service for generating avatars

---
+++ src/services/v1/AvatarGeneration.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+class AvatarGeneration {
+  async post(path, body) {
+    const response = await axios.post(
+      `${process.env.RUNNER_URL}/api/v1${path}`,
+      body,
+      { headers: { Authorization: 'Bearer ' + process.env.RUNNER_KEY } }
+    );
+    return response.data;
+  }
+
+  async generatePersonaAvatar(persona) {
+    return await this.post('/avatars/personas/generate', { persona });
+  }
+
+  async generateProblemAvatar(problem) {
+    return await this.post('/avatars/problems/generate', { problem });
+  }
+
+  async generateLeiaAvatar(leia) {
+    return await this.post('/avatars/leias/generate', { leia });
+  }
+}
+
+export default new AvatarGeneration();
