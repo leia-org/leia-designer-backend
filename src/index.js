addcc85cc058a2bd44e31d5f0e755458d42171ce
DavidLeal <davidleallopez2005@gmail.com>
2026-06-27 19:39:56 +0200

    fix: swagger allow to do post method

--- src/index.js
+++ src/index.js
@@ -18,12 +18,13 @@ import SwaggerParser from 'swagger-parser';
 import { auth } from './middlewares/auth.js';
 
 const app = express();
-
+const PORT = process.env.PORT || 3000;
 const allowedOrigins = (process.env.FRONTEND_URL || '')
   .split(',')
   .map((origin) => origin.trim())
   .filter(Boolean);
 allowedOrigins.push(process.env.WORKBENCH_FRONTEND_URL);
+allowedOrigins.push(`http://localhost:${PORT}`);
 app.use(
   cors({
     origin: (origin, callback) => {
@@ -70,7 +71,6 @@ app.use('/api/v1/provider', providerRoutesV1);
 // Error handling middleware
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
